a0a28f443a42fd3b92256953189a1b7a6b52094b
Alex Shpil <shpil93@gmail.com>
2020-05-25 22:12:00 +0300

    task5 +

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -1,27 +1,16 @@
 """Выведите n-ое число Фибоначчи, используя только временные переменные,
     циклические операторы и условные операторы. n - вводится.
 """
-
-
 def fibonacci(n):
     """Поиск числа фибоначчи.
 
     :param n: Номер числа Фибоначчи.
     :return: Число. n-ое число Фибоначчи
     """
-
-    # write your code here
-    """
-    a0 = a1 = 1
-    for i in range(n-2):
-        a0, a1 = a1, a0 + a1
-    return "N-ое число Фибоначчи: {}".format(a1)  # write return value here
-    """
-
-    if n == 0 or n == 1:
+    if n == 1 or n == 2:
         return 1
-    else:
-        return fibonacci(n-1) + fibonacci(n+1)
+    if n > 2:
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 if __name__ == '__main__':
     # здесь можно сделать ввод из консоли и проверить работу функции
