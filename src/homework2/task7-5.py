c24352fc8b3bdc93165477151362b96d11e8bb48
Alex Shpil <shpil93@gmail.com>
2020-06-07 17:24:30 +0300

    correct hom2

---
+++ src/homework2/task7-5.py
@@ -0,0 +1,28 @@
+'''
+Написать функцию, которая для произвольной введенной строки (латинский алфавит) будет определять: 
+	- количество гласных
+	- количество согласных
+	- количество пробелов
+Возвращать полученные значения функция должна через return. Вывод значений на экран должен выполнятся в основной программе
+'''
+
+
+def vowel_count(str):
+    countGl = 0
+    countSogl = 0
+    countPr = 0
+    gl = set("aeiouAEIOU")
+    sogl = set("bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ")
+    pr = set(" ")
+    for alphabet in str:
+        if alphabet in gl: #выбираем и считаем гласные
+            countGl = countGl + 1
+        elif alphabet in sogl: #выбираем и считаем согласные
+            countSogl = countSogl + 1
+        elif alphabet in pr: #выбираем и считаем пробелы
+            countPr = countPr + 1
+    return countGl, countSogl, countPr
+
+str = input("Введите строку: ")
+a, b, c = vowel_count(str)
+print("Гласные:", a, "\nСогласные:", b, "\nПробелы:", c)
