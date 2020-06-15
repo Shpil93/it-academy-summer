6088a044998658e0e33f30a2130314a56bb9692f
Alex Shpil <shpil93@gmail.com>
2020-06-15 19:13:45 +0300

    task 7-3 H2

--- src/homework2/task7-3.py
+++ src/homework2/task7-3.py
@@ -6,7 +6,7 @@ listA = list([])
 a = int(input("Введите длинну массива: "))
 b = int(input("Введите ширину массива: "))
 for i in range(a):
-    listA.append([1]*b)
+    listA.append([1] * b)
 for i in range(a):
     for j in range(b):
         if (i % 2 == 0 and j % 2 == 1) or (i % 2 == 1 and j % 2 == 0):
