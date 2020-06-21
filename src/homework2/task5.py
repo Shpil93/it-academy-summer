d3f1f5caaa8fa20c6ec5dda1766a4bc3d800baa1
Alex Shpil <shpil93@gmail.com>
2020-06-21 23:21:22 +0300

    corrected+ H2

--- src/homework2/task5.py
+++ src/homework2/task5.py
@@ -13,11 +13,11 @@ def fibonacci(n):
     if n in (1, 2):
         return 1
     else:
-        if not cache.get(n-1):
-            cache[n-1] = fibonacci(n-1)
-        if not cache.get(n-2):
-            cache[n-2] = fibonacci(n-2)
-        return cache.get(n-1) + cache.get(n-2)
+        if not cache.get(n - 1):
+            cache[n - 1] = fibonacci(n - 1)
+        if not cache.get(n - 2):
+            cache[n - 2] = fibonacci(n - 2)
+        return cache.get(n - 1) + cache.get(n - 2)
 
 
 if __name__ == '__main__':
